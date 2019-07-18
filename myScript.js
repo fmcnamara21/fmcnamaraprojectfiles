fe8f89302153126454ef14d355e2866c1f4a7ceb
fmcnamara21 <53058440+fmcnamara21@users.noreply.github.com>
2019-07-18 11:46:16 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,8 @@
+
+function myFunction() {
+  document.getElementById("header1").innerHTML = "Welcome";
+}
+
+function Function2() {
+    document.getElementById("header5").innerHTML = location.href= 'health.html';
+}
